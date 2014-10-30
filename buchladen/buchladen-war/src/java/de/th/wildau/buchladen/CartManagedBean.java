1e41cce4b6df3223d595ad7e30958d7d9bd4a517
Jan Gabler <jan.gabler@gmx.net>
2014-10-30 22:45:25 +0100

    add cart feature

--- buchladen/buchladen-war/src/java/de/th/wildau/buchladen/CartManagedBean.java
+++ buchladen/buchladen-war/src/java/de/th/wildau/buchladen/CartManagedBean.java
@@ -5,7 +5,12 @@
  */
 package de.th.wildau.buchladen;
 
+import de.th.wildau.webapp.buchladen.entities.BookEntity;
+import de.th.wildau.webapp.buchladen.sessions.CartSessionBeanRemote;
+import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
@@ -15,9 +20,11 @@ import javax.faces.context.FacesContext;
  *
  * @author Jan
  */
-@ManagedBean
-@SessionScoped
-public class CartManagedBean {
+//@ManagedBean
+//@SessionScoped
+public class CartManagedBean implements Serializable {
+    @EJB
+    private CartSessionBeanRemote cartSessionBean;
     
     /**
      * Creates a new instance of CartManagedBean
@@ -25,14 +32,19 @@ public class CartManagedBean {
     public CartManagedBean() {
     }
     
-    public void add() {
-        //...
+    public void addBook(int id) {
+        this.cartSessionBean.addBook(id);
     }
     
-    public int getId() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        ExternalContext extContext = context.getExternalContext();
-        Map<String, String> params = extContext.getRequestParameterMap();
-        return Integer.parseInt(params.get("id"));
+    public void removeBook(int id) {
+        this.cartSessionBean.removeBook(id);
+    }
+    
+    public List<BookEntity> getContent() {
+        return this.cartSessionBean.getContent();
+    }
+    
+    public int count() {
+        return this.cartSessionBean.count();
     }
 }
