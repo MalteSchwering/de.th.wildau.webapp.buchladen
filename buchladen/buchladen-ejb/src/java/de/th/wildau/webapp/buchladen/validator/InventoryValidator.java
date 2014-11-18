c19c7a911eb2427b99ea0e1d65d20560edfb32a7
gabler <gabler.jan@gmail.com>
2014-11-18 18:12:09 +0100

    add inventory validation into cart, close#7

---
+++ buchladen/buchladen-ejb/src/java/de/th/wildau/webapp/buchladen/validator/InventoryValidator.java
@@ -0,0 +1,47 @@
+package de.th.wildau.webapp.buchladen.validator;
+
+import de.th.wildau.webapp.buchladen.entities.BookEntity;
+import de.th.wildau.webapp.buchladen.facades.BookEntityFacadeRemote;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class InventoryValidator implements Validator {
+    BookEntityFacadeRemote bookEntityFacade = lookupBookEntityFacadeRemote();
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        int bookEntityId = (int) component.getAttributes().get("bookEntityId");
+        BookEntity bookEntity = this.bookEntityFacade.find(bookEntityId);
+        int bookInventory = bookEntity.getQuantity();
+        int quantityFromCart = Integer.parseInt(value.toString());
+        
+        if(bookEntity == null) {
+            FacesMessage facesMessage = new FacesMessage(component.getClientId() + ": Überprüfungsfehler: Buch existiert nicht");
+            throw new ValidatorException(facesMessage);
+        }
+        else if(quantityFromCart > bookInventory) {
+            String bookName = "\"" + bookEntity.getTitle() + " von " + bookEntity.getFkAuthorId().getFirstName() + " " + bookEntity.getFkAuthorId().getLastName() + "\"";
+            FacesMessage facesMessage = new FacesMessage("Der Lagerbestand von " + bookName + " ist zu niedrig, es sind max. " + bookInventory + " Bestellungen dieses Exemplars möglich");
+            throw new ValidatorException(facesMessage);
+        }
+    }
+
+    private BookEntityFacadeRemote lookupBookEntityFacadeRemote() {
+        try {
+            Context c = new InitialContext();
+            return (BookEntityFacadeRemote) c.lookup("java:global/buchladen/buchladen-ejb/BookEntityFacade!de.th.wildau.webapp.buchladen.facades.BookEntityFacadeRemote");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+    
+}
