8c6ed9bfeee4b499a8a857ae735168b51039e49f
Malte Schwering <malte.schwering@gmail.com>
2014-11-19 13:27:23 +0100

    included StreetNameValidator

---
+++ buchladen/buchladen-ejb/src/java/de/th/wildau/webapp/buchladen/validator/StreetNameValidator.java
@@ -0,0 +1,32 @@
+
+package de.th.wildau.webapp.buchladen.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+
+public class StreetNameValidator implements Validator{
+
+    private static final String STREETNAME_REGEX = "[\\d\\w\\s-.]{0,255}";
+    private Pattern pattern;
+    private Matcher matcher;
+
+    public StreetNameValidator() {
+        pattern = Pattern.compile(STREETNAME_REGEX);
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        matcher = pattern.matcher(value.toString());
+        if(!matcher.matches()) {
+            FacesMessage facesMessage = new FacesMessage(component.getClientId());
+            throw new ValidatorException(facesMessage);
+        }
+    }
+    
+}
