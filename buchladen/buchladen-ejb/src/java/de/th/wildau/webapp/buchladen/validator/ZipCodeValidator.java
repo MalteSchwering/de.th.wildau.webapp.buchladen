bfb58c7824c913f17f79339afca71b33416fc4a0
Malte Schwering <malte.schwering@gmail.com>
2014-11-19 12:55:47 +0100

    included ZipCodeValidator

---
+++ buchladen/buchladen-ejb/src/java/de/th/wildau/webapp/buchladen/validator/ZipCodeValidator.java
@@ -0,0 +1,32 @@
+
+package de.th.wildau.webapp.buchladen.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+
+public class ZipCodeValidator implements Validator {
+
+    private static final String ZIPCODE_REGEX = "\\d{5}";
+    private Pattern pattern;
+    private Matcher matcher;
+
+    public ZipCodeValidator() {
+        pattern = Pattern.compile(ZIPCODE_REGEX);
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        matcher = pattern.matcher(value.toString());
+        if(!matcher.matches()) {
+            FacesMessage facesMessage = new FacesMessage(component.getClientId());
+            throw new ValidatorException(facesMessage);
+        }
+    }
+    
+}
