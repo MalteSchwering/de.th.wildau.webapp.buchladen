e9a8d1c7e6cb699547a086a508d4e149ee1dc469
Malte Schwering <malte.schwering@gmail.com>
2014-12-23 12:46:50 +0100

    updated checkout for validatormessages, added IBANValidator

---
+++ buchladen/buchladen-ejb/src/java/de/th/wildau/webapp/buchladen/validator/IBANValidator.java
@@ -0,0 +1,59 @@
+package de.th.wildau.webapp.buchladen.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * @author Jan Gabler
+ * @author Malte Schwering
+ */
+public class IBANValidator implements Validator{
+
+    /**
+     * Regulärer Ausdruck des IBAN.
+     * Er verbietet alles außer ein Passwort aus mindestens 8 Buchstaben,
+     * bestehend aus Großbuchstaben, Kleinbuchstaben, Zahlen und mindestens 
+     * ein Sonderzeichen, wie zum Beispiel @#$%
+     */
+    private static final String IBAN_REGEX = "\\w{2}\\d{2} ?\\d{4} ?\\d{4} ?\\d{4} ?\\d{4} ?\\d{2}";
+
+    /**
+     * Kompilierte Repräsentation des regulären Ausdrucks.
+     */
+    private Pattern pattern;
+
+    /**
+     * Match Engine.
+     */
+    private Matcher matcher;
+
+    /**
+     * Konstruktor der den regulären Ausdruck kompiliert.
+     */
+    public IBANValidator() {
+        pattern = Pattern.compile(IBAN_REGEX);
+    }
+
+    /**
+     * Validiert den Wert des Input Elements.
+     * @param context FacesContext
+     * @param component User Interface Komponente
+     * @param value Wert des Input Elements
+     * @throws ValidatorException 
+     */
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        matcher = pattern.matcher(value.toString());
+        if(!matcher.matches()) {
+            FacesMessage facesMessage = new FacesMessage(component.getClientId());
+            throw new ValidatorException(facesMessage);
+        }
+    }
+    
+}
+
