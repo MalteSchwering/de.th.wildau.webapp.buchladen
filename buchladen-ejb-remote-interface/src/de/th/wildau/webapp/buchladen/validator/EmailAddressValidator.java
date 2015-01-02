89c2838e07771636711a6d6be390d9af8bb8a290
gabler <gabler.jan@gmail.com>
2015-01-02 14:38:52 +0100

    update location of validator classes back to ejb and split the email address validator, #54

--- buchladen-ejb-remote-interface/src/de/th/wildau/webapp/buchladen/validator/EmailAddressValidator.java
+++ buchladen-ejb-remote-interface/src/de/th/wildau/webapp/buchladen/validator/EmailAddressValidator.java
@@ -1,8 +1,5 @@
 package de.th.wildau.webapp.buchladen.validator;
 
-import de.th.wildau.webapp.buchladen.facades.RegisteredUserEntityFacadeRemote;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.faces.application.FacesMessage;
@@ -10,9 +7,6 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 /**
  * @author Jan Gabler
@@ -20,12 +14,7 @@ import javax.naming.NamingException;
  * @version 0.1
  */
 public class EmailAddressValidator implements Validator {
-    
-    /**
-     * Enterprise Java Bean registeredUserEntityFacade.
-     */
-    RegisteredUserEntityFacadeRemote registeredUserEntityFacade = lookupRegisteredUserEntityFacadeRemote();
-    
+        
     /**
      * Regulärer Ausdruck der E-Mail Adresse.
      * Er verbietet alles außer einer normalen E-Mail-Adresse.
@@ -67,25 +56,6 @@ public class EmailAddressValidator implements Validator {
         if(value.toString().length() > 250) {
             FacesMessage facesMessage = new FacesMessage(component.getClientId());
             throw new ValidatorException(facesMessage);
-        }
-        
-        if(registeredUserEntityFacade.findByEmailAddress(value.toString()) != null) {
-            FacesMessage facesMessage = new FacesMessage(component.getClientId());
-            throw new ValidatorException(facesMessage);
-        }
-    }
-    
-    /**
-     * Liefert die Remote registeredUserEntityFacade zurück.
-     * @return registeredUserEntityFacade
-     */
-    private RegisteredUserEntityFacadeRemote lookupRegisteredUserEntityFacadeRemote() {
-        try {
-            Context c = new InitialContext();
-            return (RegisteredUserEntityFacadeRemote) c.lookup("java:global/buchladen/buchladen-ejb/RegisteredUserEntityFacade!de.th.wildau.webapp.buchladen.facades.RegisteredUserEntityFacadeRemote");
-        } catch (NamingException ne) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
-            throw new RuntimeException(ne);
         }
     }
     
---
+++ buchladen/buchladen-ejb/src/java/de/th/wildau/webapp/buchladen/validator/EmailAddressDuplicateValidator.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.th.wildau.webapp.buchladen.validator;
+
+import de.th.wildau.webapp.buchladen.facades.RegisteredUserEntityFacadeRemote;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * @author Jan Gabler
+ * @author Malte Schwering
+ * @version 0.1
+ */
+public class EmailAddressDuplicateValidator implements Validator {
+    
+    /**
+     * Enterprise Java Bean registeredUserEntityFacade.
+     */
+    RegisteredUserEntityFacadeRemote registeredUserEntityFacade = lookupRegisteredUserEntityFacadeRemote();
+    
+    /**
+     * Validiert den Wert des Input Elements auf Duplikate.
+     * @param context
+     * @param component
+     * @param value
+     * @throws ValidatorException 
+     */
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        if(registeredUserEntityFacade.findByEmailAddress(value.toString()) != null) {
+            FacesMessage facesMessage = new FacesMessage(component.getClientId());
+            throw new ValidatorException(facesMessage);
+        }
+    }
+    
+    /**
+     * Liefert die Remote registeredUserEntityFacade zurück.
+     * @return registeredUserEntityFacade
+     */
+    private RegisteredUserEntityFacadeRemote lookupRegisteredUserEntityFacadeRemote() {
+        try {
+            Context c = new InitialContext();
+            return (RegisteredUserEntityFacadeRemote) c.lookup("java:global/buchladen/buchladen-ejb/RegisteredUserEntityFacade!de.th.wildau.webapp.buchladen.facades.RegisteredUserEntityFacadeRemote");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+    
+}
